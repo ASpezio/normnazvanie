b0bd2dd8d70d3ba1890495cba0b94cd1f2146d25
skifyh <119287462+skifyh@users.noreply.github.com>
2023-01-25 15:22:04 +0500

    Add files via upload

---
+++ EfremenkoGeom.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public class Math
+    {
+        public double a; // основание 
+        public Math()
+        {
+            this.a = 0;
+        }
+        public Math(double a)
+        {
+            this.a = a;
+        }
+        public double PlRavnTr()
+        {
+            return (a^2 * Math.sqrt(3)) / 4;
+        }
+        public static double PlRavnTr(double a)
+        {
+            return (this.a^2 * Math.sqrt(3)) / 4;
+        }
+        public double PlPravShest()
+        {
+            return (3 * a^2 * Math.sqrt(3)) / 2;
+        }
+        public static double PlPravShest(double a)
+        {
+            return (3 * this.a^2 * Math.sqrt(3)) / 2;
+        }
+
+    }
+}
